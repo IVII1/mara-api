a16de5ba7a1ff7d5fa17b673ce66bd2bafe1899e
Ivan Jovanovic <ivojlo2002@gmail.com>
2025-02-13 12:19:26 +0100

    moved register route to web

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register'])->name('user.register');
+
 Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 Route::get('/projects', [ProjectController::class, 'index'])->name('project.get.all');
 Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('project.find');
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
     return response()->json(['message' => 'Hello']);
 });
 
+Route::post('/register', [AuthController::class, 'register'])->name('user.register');
